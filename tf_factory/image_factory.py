ac6d930428baa8201343c5bbe3edc4bf1a77ca2e
fstrub <florian.strub@gmail.com>
2017-10-06 02:49:13 -0400

    fix no_units for cbn pluggin

--- tf_factory/image_factory.py
+++ tf_factory/image_factory.py
@@ -26,7 +26,7 @@ def get_image_features(image, question, is_training, scope_name, config):
             # Create CBN
             cbn = None
             if config["cbn"].get("use_cbn", False):
-                cbn_factory = CBNfromLSTM(question, config['cbn'])
+                cbn_factory = CBNfromLSTM(question, no_units=config['cbn']["cbn_embedding_size"])
 
                 excluded_scopes = config["cbn"].get('excluded_scope_names', [])
                 cbn = ConditionalBatchNorm(cbn_factory, excluded_scope_names=excluded_scopes,
