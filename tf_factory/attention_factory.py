19d04f54eea6179715555e1360f05ab4fbc3528c
fstrub <florian.strub@gmail.com>
2017-10-06 00:52:14 -0400

    change packaging

---
+++ tf_factory/attention_factory.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+
+from neural_toolbox.attention import compute_attention, compute_glimpse
+
+def get_attention(feature_map, lstm, config, keep_dropout=1):
+    attention_mode = config.get("mode", None)
+
+    if attention_mode == "none":
+        picture_out = feature_map
+    if attention_mode == "mean":
+        picture_out = tf.reduce_mean(feature_map, axis=(1, 2))
+    elif attention_mode == "classic":
+        picture_out = compute_attention(feature_map,
+                                        lstm,
+                                        no_mlp_units=config['no_attention_mlp'])
+    elif attention_mode == "glimpse":
+        picture_out = compute_glimpse(feature_map,
+                                      lstm,
+                                      no_glims=config['no_glimpses'],
+                                      glimse_embedding_size=config['no_attention_mlp'],
+                                      keep_dropout=keep_dropout)
+    else:
+        assert False, "Wrong attention mode: {}".format(attention_mode)
+
+    return picture_out
--- tf_factory/image_factory.py
+++ tf_factory/image_factory.py
@@ -1,12 +1,10 @@
 import tensorflow as tf
 
-# Note that those packages break dependencies
-from conditional_batch_norm.cbn_pluggin import CBNfromLSTM
-from conditional_batch_norm.conditional_bn import ConditionalBatchNorm
-from conditional_batch_norm.resnet import create_resnet
+from neural_toolbox.cbn import CBNfromLSTM
+from neural_toolbox.resnet import create_resnet
+from neural_toolbox.cbn import ConditionalBatchNorm
 
-from generic.tf_models import attention
-
+from generic.tf_factory.attention_factory import get_attention
 
 def get_image_features(image, question, is_training, scope_name, config):
     image_input_type = config["image_input"]
@@ -51,7 +49,7 @@ def get_image_features(image, question, is_training, scope_name, config):
             image_feature_maps = image
 
         # apply attention
-        image_out = attention.attention_factory(image_feature_maps, question, config["attention"])
+        image_out = get_attention(image_feature_maps, question, config["attention"])
 
     else:
         assert False, "Wrong input type for image"
